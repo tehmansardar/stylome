8d30453e9a942dc2da0bf43aeee8a1e410332a5e
Tehman Sardar <tehmansardar@gmail.com>
2021-06-17 04:16:38 +0500

    Add staff beta

--- client/src/src/screens/DashboardScreens/Staff/index.js
+++ client/src/src/screens/DashboardScreens/Staff/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { Container, Grid } from '@material-ui/core';
 import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -20,6 +21,15 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+import './style.css';
 
 function createData(name, calories, fat, carbs, protein) {
 	return { name, calories, fat, carbs, protein };
@@ -166,60 +176,13 @@ const useToolbarStyles = makeStyles((theme) => ({
 	},
 }));
 
-const EnhancedTableToolbar = (props) => {
-	const classes = useToolbarStyles();
-	const { numSelected } = props;
-
-	return (
-		<Toolbar
-			className={clsx(classes.root, {
-				[classes.highlight]: numSelected > 0,
-			})}
-		>
-			{numSelected > 0 ? (
-				<Typography
-					className={classes.title}
-					color='inherit'
-					variant='subtitle1'
-					component='div'
-				>
-					{numSelected} selected
-				</Typography>
-			) : (
-				<Typography
-					className={classes.title}
-					variant='h6'
-					id='tableTitle'
-					component='div'
-				>
-					Stylome Staff
-				</Typography>
-			)}
-
-			{numSelected > 0 ? (
-				<Tooltip title='Delete'>
-					<IconButton aria-label='delete'>
-						<DeleteIcon />
-					</IconButton>
-				</Tooltip>
-			) : (
-				<Tooltip title='Filter list'>
-					<IconButton aria-label='filter list'>
-						<FilterListIcon />
-					</IconButton>
-				</Tooltip>
-			)}
-		</Toolbar>
-	);
-};
-
-EnhancedTableToolbar.propTypes = {
-	numSelected: PropTypes.number.isRequired,
-};
-
 const useStyles = makeStyles((theme) => ({
 	root: {
 		width: '100%',
+	},
+	container: {
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(4),
 	},
 	paper: {
 		width: '100%',
@@ -242,6 +205,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Staff = () => {
+	const services = useSelector((state) => state.salons.services);
+
+	const [service, setService] = React.useState('');
+	const handleChange = (event) => {
+		setService(event.target.value);
+	};
+
+	console.log(service);
+
 	const classes = useStyles();
 	const [order, setOrder] = React.useState('asc');
 	const [orderBy, setOrderBy] = React.useState('calories');
@@ -303,81 +275,141 @@ const Staff = () => {
 	const emptyRows =
 		rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
+	const EnhancedTableToolbar = (props) => {
+		const classes = useToolbarStyles();
+		const { numSelected } = props;
+
+		return (
+			<Toolbar
+				className={clsx(classes.root, {
+					[classes.highlight]: numSelected > 0,
+				})}
+			>
+				{/* <Typography
+					className={classes.title}
+					variant='h6'
+					id='tableTitle'
+					component='div'
+				>
+					Stylome Staff
+				</Typography> */}
+
+				<div>
+					<form className='staff flex'>
+						<TextField
+							id='outlined-basic'
+							label='Add New staff'
+							className={classes.textField}
+							// size='small'
+							variant='outlined'
+						/>
+
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={service}
+							onChange={handleChange}
+							// size='small'
+							// autoWidth='false'
+							className='service'
+							variant='outlined'
+						>
+							<MenuItem value=''>Add Service</MenuItem>
+							{services.map((service) => (
+								<MenuItem value={service._id}>{service.service}</MenuItem>
+							))}
+						</Select>
+
+						<Button variant='contained' color='primary' size='small'>
+							Add Staff
+						</Button>
+					</form>
+				</div>
+			</Toolbar>
+		);
+	};
+
+	EnhancedTableToolbar.propTypes = {
+		numSelected: PropTypes.number.isRequired,
+	};
+
 	return (
 		<div className={classes.root}>
 			{/* <Paper className={classes.paper}> */}
-			<EnhancedTableToolbar numSelected={selected.length} />
-			<TableContainer>
-				<Table
-					className={classes.table}
-					aria-labelledby='tableTitle'
-					size={dense ? 'small' : 'medium'}
-					aria-label='enhanced table'
-				>
-					<EnhancedTableHead
-						classes={classes}
-						numSelected={selected.length}
-						order={order}
-						orderBy={orderBy}
-						onSelectAllClick={handleSelectAllClick}
-						onRequestSort={handleRequestSort}
-						rowCount={rows.length}
-					/>
-					<TableBody>
-						{stableSort(rows, getComparator(order, orderBy))
-							.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-							.map((row, index) => {
-								const isItemSelected = isSelected(row.name);
-								const labelId = `enhanced-table-checkbox-${index}`;
+			<Container className={classes.container}>
+				<EnhancedTableToolbar numSelected={selected.length} />
+				<TableContainer>
+					<Table
+						className={classes.table}
+						aria-labelledby='tableTitle'
+						size={dense ? 'small' : 'medium'}
+						aria-label='enhanced table'
+					>
+						<EnhancedTableHead
+							classes={classes}
+							numSelected={selected.length}
+							order={order}
+							orderBy={orderBy}
+							onSelectAllClick={handleSelectAllClick}
+							onRequestSort={handleRequestSort}
+							rowCount={rows.length}
+						/>
+						<TableBody>
+							{stableSort(rows, getComparator(order, orderBy))
+								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+								.map((row, index) => {
+									const isItemSelected = isSelected(row.name);
+									const labelId = `enhanced-table-checkbox-${index}`;
 
-								return (
-									<TableRow
-										hover
-										onClick={(event) => handleClick(event, row.name)}
-										role='checkbox'
-										aria-checked={isItemSelected}
-										tabIndex={-1}
-										key={row.name}
-										selected={isItemSelected}
-									>
-										<TableCell padding='checkbox'>
-											<Checkbox
-												checked={isItemSelected}
-												inputProps={{ 'aria-labelledby': labelId }}
-											/>
-										</TableCell>
-										<TableCell
-											component='th'
-											id={labelId}
-											scope='row'
-											padding='none'
+									return (
+										<TableRow
+											hover
+											onClick={(event) => handleClick(event, row.name)}
+											role='checkbox'
+											aria-checked={isItemSelected}
+											tabIndex={-1}
+											key={row.name}
+											selected={isItemSelected}
 										>
-											{row.name}
-										</TableCell>
-										<TableCell align='right'>{row.calories}</TableCell>
-										<TableCell align='right'>{row.fat}</TableCell>
-										<TableCell align='right'>{row.carbs}</TableCell>
-										<TableCell align='right'>{row.protein}</TableCell>
-									</TableRow>
-								);
-							})}
-						{emptyRows > 0 && (
-							<TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-								<TableCell colSpan={6} />
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<TablePagination
-				rowsPerPageOptions={[5, 10, 25]}
-				component='div'
-				count={rows.length}
-				rowsPerPage={rowsPerPage}
-				page={page}
-				onChangePage={handleChangePage}
-				onChangeRowsPerPage={handleChangeRowsPerPage}
-			/>
+											<TableCell padding='checkbox'>
+												<Checkbox
+													checked={isItemSelected}
+													inputProps={{ 'aria-labelledby': labelId }}
+												/>
+											</TableCell>
+											<TableCell
+												component='th'
+												id={labelId}
+												scope='row'
+												padding='none'
+											>
+												{row.name}
+											</TableCell>
+											<TableCell align='right'>{row.calories}</TableCell>
+											<TableCell align='right'>{row.fat}</TableCell>
+											<TableCell align='right'>{row.carbs}</TableCell>
+											<TableCell align='right'>{row.protein}</TableCell>
+										</TableRow>
+									);
+								})}
+							{emptyRows > 0 && (
+								<TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+									<TableCell colSpan={6} />
+								</TableRow>
+							)}
+						</TableBody>
+					</Table>
+				</TableContainer>
+				<TablePagination
+					rowsPerPageOptions={[5, 10, 25]}
+					component='div'
+					count={rows.length}
+					rowsPerPage={rowsPerPage}
+					page={page}
+					onChangePage={handleChangePage}
+					onChangeRowsPerPage={handleChangeRowsPerPage}
+				/>
+			</Container>
 			{/* </Paper> */}
 			{/* <FormControlLabel
 				control={<Switch checked={dense} onChange={handleChangeDense} />}
