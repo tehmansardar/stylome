c0d629d5218d43b301cba187a385f8470344a6a7
Tehman Sardar <tehmansardar@gmail.com>
2021-07-12 05:19:36 +0500

    Setting routes for salon

--- client/src/src/screens/InfoSalon/MapLocation/index.js
+++ client/src/src/screens/InfoSalon/MapLocation/index.js
@@ -1,50 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-// const location = {
-// 	address: '1600 Amphitheatre Parkway, Mountain View, california.',
-// 	lat: 37.42216,
-// 	lng: -122.08427,
-// };
-
-// eslint-disable-next-line no-restricted-globals
 const MapLocation = ({ loc }) => {
+	const [userLocation, setUserLocation] = useState({ lat: null, lng: null });
+
 	let latLng = loc.latLng;
 	latLng = latLng.split(',');
 
-	const locations = {
-		address: loc.address,
-		lat: parseFloat(latLng[0]),
-		lng: parseFloat(latLng[1]),
+	useEffect(() => {
+		function getLocation() {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(showPosition);
+			} else {
+				console.log('Geolocation is not supported by this browser.');
+			}
+		}
+		getLocation();
+
+		function showPosition(position) {
+			setUserLocation({
+				...userLocation,
+				lat: position.coords.latitude,
+				lng: position.coords.longitude,
+			});
+		}
+	}, []);
+
+	const currentLocation = {
+		lat: userLocation.lat,
+		lng: userLocation.lng,
 	};
-	console.log(locations);
 
-	const LocationPin = ({ text }) => (
-		<div className='pin'>
-			<img
-				src='https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_location_on_48px-512.png'
-				alt=''
-				style={{ width: '30px', height: '30px' }}
-			/>
-			<p style={{ color: 'black', width: '150px' }}>{text}</p>
-		</div>
-	);
+	const apiIsLoaded = (map, maps) => {
+		const directionsService = new window.google.maps.DirectionsService();
+		const directionsRenderer = new window.google.maps.DirectionsRenderer({
+			polylineOptions: { strokeColor: '#000' },
+			suppressMarkers: true,
+		});
+		directionsRenderer.setMap(map);
+		const origin = { lat: userLocation.lat, lng: userLocation.lng };
+		const destination = {
+			lat: parseFloat(latLng[0]),
+			lng: parseFloat(latLng[1]),
+		};
 
+		// 30.661422499999997, 73.1062267
+
+		directionsService.route(
+			{
+				origin: origin,
+				destination: destination,
+				travelMode: window.google.maps.TravelMode.DRIVING,
+			},
+			(result, status) => {
+				if (status === window.google.maps.DirectionsStatus.OK) {
+					directionsRenderer.setDirections(result);
+				} else {
+					console.error(`error fetching directions ${result}`);
+				}
+			}
+		);
+	};
 	return (
 		<div className='h-screen w-2/4'>
 			<GoogleMapReact
-				bootstrapURLKeys={{ key: 'AIzaSyBelvJjnjNr3A30X5dTVEMmoilV6FH_XP0' }}
-				defaultCenter={locations}
-				defaultZoom={17}
-			>
-				<LocationPin
-					lat={locations.lat}
-					lng={locations.lng}
-					text={locations.address}
-				/>
-			</GoogleMapReact>
+				bootstrapURLKeys={{
+					key: 'AIzaSyBelvJjnjNr3A30X5dTVEMmoilV6FH_XP0',
+				}}
+				defaultCenter={{
+					lat: userLocation.lat,
+					lng: userLocation.lng,
+				}}
+				defaultZoom={11}
+				center={currentLocation}
+				yesIWantToUseGoogleMapApiInternals
+				onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps)}
+			/>
 		</div>
 	);
 };
-
 export default MapLocation;
